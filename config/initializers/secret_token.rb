41c10967b8e25aeb46524d719ca9634a24bb7b86
Dave Sexton <davesexton1@googlemail.com>
2011-12-03 17:17:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Butterflyicing::Application.config.secret_token = '2f80c6d9ba00474e00a1913d445e390f442906ab18bf698092c7a101d4d9af8e1c394c5ba58d4f564e00f07948c2d21250f24ac95b260a74f8e68c08afb89fb5'
