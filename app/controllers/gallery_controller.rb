9f4aa8843f86f51b999331fac1bbf6a1c9b2fcc8
Dave Sexton <davesexton1@googlemail.com>
2013-11-14 08:52:49 +0000

    corrected spelling errors

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -4,10 +4,6 @@ class GalleryController < ApplicationController
   def index
 
     redirect_to gallery_path(Tag.first.id) unless params[:id]
-#    tg = Tag.includes(:gallery_images)
-#            .group('tags.id')
-#            .having('COUNT(gallery_images.id) > 0').map{|t| t.tag_group_id}
-#    @tag_groups = TagGroup.find(tg)
 
     @tags = Tag.includes(:gallery_images)
                .group('tags.id')
